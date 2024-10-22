da27487384f3dd26d1baf80487b995558a624d3c
Lualinn <lualinnq@gmail.com>
2024-10-23 02:07:28 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = tuple_ = 3, 'kakis'
+print(immutable_var)
+#tuple_[0] = 69         Кортеж является неизменным и не поддерживает обращение по элементам.
+mutable_list = list_ = [69, 'pukis']
+print(mutable_list)
+list_[0] = 11
+print(mutable_list)
