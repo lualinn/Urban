30cd55db4553d3ee97987fa8987f5f1988664cd4
Lualinn <lualinnq@gmail.com>
2024-10-25 23:54:59 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+done = 12
+hours_spent = 1.5
+lesson = "Python"
+average_time = (1.5 / 12)
+print(lesson, 'всего задач:', done, 'затрачено часов:', hours_spent, 'среднее время выполнения', average_time, 'часа.')
