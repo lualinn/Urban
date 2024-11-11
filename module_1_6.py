24495134e83ac0086f9d552b6241a73944c8fdbb
Lualinn <lualinnq@gmail.com>
2024-11-11 17:44:29 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -6,3 +6,9 @@ my_dict.update({'Oxana': 1987,
 print(my_dict)
 print(my_dict.pop('Marina'))
 print(my_dict)
+my_set = {1, 2, 3, 'apple', 'banana', 1, 2, 'apple', 3.5, True, False, None}
+print("Первоначальное множество my_set:", my_set)
+my_set.add('orange')
+my_set.add(10)
+my_set.discard(1)
+print("Измененное множество my_set:", my_set)
